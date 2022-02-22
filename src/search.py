b146aa1e63465b5263537fd5c4e088f887f7f87b
ezeltinger <ezeltinger@gmail.com>
2022-02-22 17:37:38 -0600

    Add tracking of users through movement

--- src/search.py
+++ src/search.py
@@ -3,41 +3,40 @@ from matplotlib.axes import Axes
 from statistics import mean
 from users import User
 from typing import Union, Sequence
-from plotting import plot_beam
+from plotting import plot_beams
 
-def search_sector(start_angle, end_angle, users):
+def send_beam(start_angle, end_angle, users, cell_radius):
     ack = 0
     for user in users:
-        if (user.theta >= start_angle) and (user.theta <= end_angle):
+        if (user.theta >= start_angle) and (user.theta <= end_angle) and (user.radius <= cell_radius):
             ack = ack + 1
     return ack
 
-def round_robin_search(sectors: np.ndarray, radius, users: Union[User, Sequence[User]], tolerance, ax: Axes):
-    sector_list = []
+def round_robin_search(beams: np.ndarray, cell_radius, users: Union[User, Sequence[User]], tolerance, ax: Axes):
+    beam_list = []
     marked_angles = []
-    # Search through sectors once and mark sectors with acknowledgements
-    for i in range(0, sectors.size - 1):
-        acks = search_sector(sectors[i], sectors[i+1], users)
+    # Search through beams once and mark beams with acknowledgements
+    for i in range(0, beams.size - 1):
+        acks = send_beam(beams[i], beams[i+1], users, cell_radius)
         if acks != 0:
-            marked_angles.append((sectors[i], sectors[i+1], acks))
-    sector_list = marked_angles
+            marked_angles.append((beams[i], beams[i+1], acks))
+    beam_list = marked_angles
 
-    # Divide and search sectors until sector width tolerance is met
-    while sector_list[0][1] - sector_list[0][0] > tolerance:
+    # Divide and search beams until beam width tolerance is met
+    while beam_list[0][1] - beam_list[0][0] > tolerance:
         marked_angles = []
-        for start_angle, end_angle, acks in sector_list:
+        for start_angle, end_angle, acks in beam_list:
             # Divide angles into 2 angles
             mid_angle = mean([start_angle, end_angle])
-            acks1 = search_sector(start_angle, mid_angle, users)
-            acks2 = search_sector(mid_angle, end_angle, users)
+            acks1 = send_beam(start_angle, mid_angle, users, cell_radius)
+            acks2 = send_beam(mid_angle, end_angle, users, cell_radius)
             if acks1:
                 marked_angles.append((start_angle, mid_angle, acks1))
             if acks2:
                 marked_angles.append((mid_angle, end_angle, acks2))
-        sector_list = marked_angles
+        beam_list = marked_angles
 
-    for start_angle, end_angle, _ in sector_list:
-        # Plot the beam where the user is found
-        plot_beam((start_angle, end_angle), radius, ax)
-    print(sector_list)
-    return sector_list
+    # Plot the beam where the user is found
+    beam_plots = plot_beams(beam_list, cell_radius, ax)
+    print(beam_list)
+    return beam_list, beam_plots
--- src/users.py
+++ src/users.py
@@ -1,13 +1,57 @@
 import numpy as np
 from numpy.random import rand
 import matplotlib.pyplot as plt
+from brownian import brownian
+from typing import Union, Sequence
+
+class UserError(Exception):
+    pass
+
+
+def cart2pol(x, y):
+    """Converts cartesian coordinates to polar coordinates"""
+    rho = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    # arctan2 returns values on [-pi, pi], we want values on [0, 2pi]
+    if theta < 0:
+        theta = 2*np.pi + theta
+    return(rho, theta)
+
+def pol2cart(rho, theta):
+    """Converts polar coordinates to cartesian coordinates"""
+    x = rho * np.cos(theta)
+    y = rho * np.sin(theta)
+    return(x, y)
 
 class User:
-
+    """Class for tracking user position
+    """
     def __init__(self, radius=None, theta=None):
         self.radius = radius if radius is not None else rand()
         self.theta = theta if theta is not None else 2*np.pi*rand()
-        self.x = self.radius*np.cos(self.theta)
-        self.y = self.radius*np.sin(self.theta)
+        self.x, self.y = pol2cart(self.radius, self.theta)
+        self.path = None
 
-    
+    def create_path(self, time, steps, delta=0.25):
+        dt = time/steps
+        self.path = np.empty((2, steps+1))
+        self.path[0, 0] = self.x
+        self.path[1, 0] = self.y
+        brownian(self.path[:,0], steps, dt, delta, out=self.path[:,1:])
+
+    def move(self, step):
+        if self.path is None:
+            raise UserError("Need to create path before moving along path.")
+        elif step>self.path.shape[1]:
+            print(f"Length of path: {self.path.shape}")
+            raise UserError("Can't go to step that is farther than path is defined.")
+        else:
+            self.x = self.path[0, step]
+            self.y = self.path[1, step]
+            self.radius, self.theta = cart2pol(self.x, self.y)
+
+
+def move_users(users: Union[User, Sequence[User]], step):
+    for user in users:
+        user.move(step)
+        
