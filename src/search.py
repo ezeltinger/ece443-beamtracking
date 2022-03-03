f2a2ab3b55828bbd7cd8fc485b959f3286a2042a
Ethan Zeltinger <ezeltinger@gmail.com>
2022-03-02 22:59:21 -0600

    fix upper bound search algorithm

--- src/search.py
+++ src/search.py
@@ -18,19 +18,29 @@ def send_beam(beam: Beam, users: Union[User, Sequence[User]], cell_radius):
         raise UserError(f"Can not search for objects of type {type(users)}")
     return ack
 
-def find_overlap(ack_beams: Sequence[Beam], non_ack_beams: Sequence[Beam], beam_count):
-    if ack_beams:
-        # find the end angle of the first ack beam
-        end_angle = ack_beams[0].end_angle
-        # find the start angle of the last ack beam
-        start_angle = ack_beams[-1].start_angle
-        if non_ack_beams:
-            if non_ack_beams[-1].end_angle >= end_angle:
-                end_angle = non_ack_beams[-1].start_angle
+def find_beam(beam_ack_list: Sequence[Beam], beam_count):
+    # Check for acks
+    if [beam_ack for beam_ack in beam_ack_list if beam_ack[1] == 1]:
+        # start with the case where all beams return an ack
+        start_angle = beam_ack_list[-1][0].start_angle
+        end_angle = beam_ack_list[0][0].end_angle
+        # We can't change ack state until we've tried more than one beam
+        last_ack = beam_ack_list[0][1]
+        for beam, ack in beam_ack_list:
+            if ack == last_ack:
+                last_ack == ack
+                last_beam = beam
             else:
-                start_angle = non_ack_beams[-1].end_angle
+                if last_ack == 1:
+                    start_angle = last_beam.start_angle
+                    end_angle = beam.start_angle
+                    break
+                else:
+                    start_angle = last_beam.end_angle
+                    end_angle = beam.end_angle
+                    break
         return Beam(start_angle, end_angle)
-    else:
+    else: # There are no acks
         return Beam(2*np.pi - np.pi/beam_count, 2*np.pi)
 
 def exhaustive_search(cell: Cell, users: Union[User, Sequence[User]], beam_count):
@@ -48,15 +58,11 @@ def exhaustive_search(cell: Cell, users: Union[User, Sequence[User]], beam_count
     return beam_list
 
 def upper_bound_search(cell: Cell, users: User, beam_count):
-    ack_beams = []
-    non_ack_beams = []
+    beam_ack_list = []
     for i in range(beam_count):
         beam = Beam(i*np.pi/beam_count, np.pi + i*np.pi/beam_count)
         # Check Beam and mark with acknowledgements
         ack = send_beam(beam, users, cell.radius)
-        if ack:
-            ack_beams.append(beam)
-        else:
-            non_ack_beams.append(beam)
+        beam_ack_list.append((beam, ack))
     
-    return find_overlap(ack_beams, non_ack_beams, beam_count)
+    return find_beam(beam_ack_list, beam_count)
